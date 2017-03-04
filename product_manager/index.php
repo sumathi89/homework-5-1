d6975ee0f03a9085cbf975896a933bdc441a82f7
Sumathi Selvaraj <ss3467@afsconnect1.njit.edu>
2017-03-03 20:58:38 -0500

    2.Display categories

--- product_manager/index.php
+++ product_manager/index.php
@@ -36,7 +36,9 @@ if ($action == 'list_products') {
     }
 } else if ($action == 'show_add_form') {
     $categories = get_categories();
-    include('product_add.php');    
+    include('product_add.php'); 
+} else if ($action=='list_categories') {
+    include('category_list.php');
 } else if ($action == 'add_product') {
     $category_id = filter_input(INPUT_POST, 'category_id', 
             FILTER_VALIDATE_INT);
